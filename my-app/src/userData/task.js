4f83e8b0f8062fde4c3224604b10ca195cee3134
shiweiding <zittery@163.com>
2020-12-19 17:08:50 -0500

    new patch

--- my-app/src/userData/task.js
+++ my-app/src/userData/task.js
@@ -1,10 +1,29 @@
 const dbConnection = require('../config/mongoConnection');
 const Function = require('./users');
+const userExample = {
+    name: "admin",
+    email:"admin@admin.com",
+    password:"admin"
+};
+const commentlist = [
+    {
+        title: "Example1",
+        content: "Example1"
+    },
+    {
+        title: "Example2",
+        content: "Example2"
+    }
+];
+const exampleLikenum = 10
 async function main() {
     const db = await dbConnection();
     await db.dropDatabase();
-    const like = await Function.SetAllLike();
-    console.log(like);
+    const user = await Function.addUser(userExample.name,userExample.email,userExample.password);
+    const like = await Function.SetAllLike(exampleLikenum);
+    for (var i = 0; i < commentlist.length; i++) {
+        const comment = await Function.addComment(commentlist[i]);
+    }
     console.log('Done seeding database');
     await db.serverConfig.close();
 }
